72185dd69b4d71e8101ad0dea6541df383cb2f05
Regislaine <nanafidelis@gmail.com>
2022-10-23 14:11:53 -0300

    refactor: Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ import Footer from './Footer.jsx';
 
 export default function Home() {
  return (
-    <div><Header />
+    <div className='home'><Header />
       <div className="main">
           <div className="main-card">
             <img src={foto} 
             alt="regislaine" width="250" height="250" className="main-foto" />
             <h1 className="title-main">Regislaine Fidelis Regis</h1>
             <h6 className="title-text-main">Campinas -SP</h6></div>
-          <div className="main-paragrafo main-card">
+          <div className="main-card main-paragrafo">
             <p> Tenho 38 anos, sou casada, adoro um bom café, tenho uma filha chamada Amora 
               buldogue inglês sou apaixonada por ela e por qualquer doguinho que encontro por aí.
             </p>
