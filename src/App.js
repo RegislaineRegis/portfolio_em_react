a9e1792ab89d7e745da1d015c9b3901aa6bb0802
Regislaine <nanafidelis@gmail.com>
2022-10-23 14:10:54 -0300

    refactor: App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import Habilidade from './components/Habilidades.jsx';
+import Contato from './components/Contato.jsx';
+import Obrigado from './components/Obrigado.jsx';
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
           <Route path="/habilidades" element = { <Habilidade />} />
+          <Route path="/contato" element = { <Contato />} />
+          <Route path="/obrigado" element = { <Obrigado />} />
           <Route exact path="/" element = { <Home /> } />
         </Routes>
       </BrowserRouter>
